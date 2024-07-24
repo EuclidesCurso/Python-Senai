5c63aefae430d7c22415984ec017c3cfb59c9a8d
EuclidesCurso <euclides.curso@gmail.com>
2024-07-24 17:35:12 -0300

    Dashboard

---
+++ dashboard.py
@@ -0,0 +1,43 @@
+import streamlit as st #criar dashboard
+import pandas as pd #manipular dados
+import plotly.express as px #construir gráficos
+
+st.set_page_config(layout="wide")
+df = pd.read_csv("supermarketSales.csv", sep=";", decimal=",")
+
+#executar no terminal streamlit run dashboard.pY
+
+#Perguntas:
+#Combobox: com o mês/ano (visão mensal de vendas)
+#Faturamento da unidade
+#Tipo de produto vendido
+#Contribuição por filial
+#Desempenho da forma de pagamento
+#Como foram as avaliações por flilais
+
+df["Date"]=pd.to_datetime(df["Date"])
+df=df.sort_values(["Date"])
+
+#visão mensal e criar um controlador por combobox
+df["Month"]=df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+
+month=st.sidebar._selectbox("Mês",df["Month"].unique())
+
+df_filter = df[df["Month"]==month]
+
+#df_filter
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
+
+fig_date = px.bar(df_filter, x="Date", y="Total", color="City", barmode="group", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width= True)
+
+fig_prod = px.bar(df_filter, x="Date", y="Product line", color="City", barmode="group", title="Faturamento por tipo de produto")
+col2.plotly_chart(fig_prod, use_container_width= True)
+
+city_total = df_filter.groupby("City")[("Total")].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="Total", barmode="group", title="Faturamento por filial")
+col3.plotly_chart(fig_city, use_container_width= True)
+
+fig_kind = px.pie(df_filter, values="Total", names="Payment", title="Faturamento por tipo pagamento")
+col4.plotly_chart(fig_kind, use_container_width= True)
