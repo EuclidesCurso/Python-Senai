1b35111d33605c18e9f5ebc367e152f7bc87bed7
EuclidesCurso <euclides.curso@gmail.com>
2024-07-25 17:32:21 -0300

    Dashboard com gráfico em pizza

--- dashboard.py
+++ dashboard.py
@@ -21,7 +21,7 @@ df=df.sort_values(["Date"])
 #visão mensal e criar um controlador por combobox
 df["Month"]=df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
 
-month=st.sidebar._selectbox("Mês",df["Month"].unique())
+month=st.sidebar.selectbox("Mês",df["Month"].unique())
 
 df_filter = df[df["Month"]==month]
 
@@ -32,12 +32,18 @@ col3, col4, col5 = st.columns(3)
 fig_date = px.bar(df_filter, x="Date", y="Total", color="City", barmode="group", title="Faturamento por dia")
 col1.plotly_chart(fig_date, use_container_width= True)
 
-fig_prod = px.bar(df_filter, x="Date", y="Product line", color="City", barmode="group", title="Faturamento por tipo de produto")
+fig_prod = px.bar(df_filter, x="Date", y="Product line", color="City", barmode="group", title="Faturamento por tipo de produto", orientation ="h")
 col2.plotly_chart(fig_prod, use_container_width= True)
 
 city_total = df_filter.groupby("City")[("Total")].sum().reset_index()
+
 fig_city = px.bar(city_total, x="City", y="Total", barmode="group", title="Faturamento por filial")
 col3.plotly_chart(fig_city, use_container_width= True)
 
-fig_kind = px.pie(df_filter, values="Total", names="Payment", title="Faturamento por tipo pagamento")
+df_filter = df[df["Month"]==month]
+fig_kind = px.pie(df_filter, values="Quantity", names="Payment", title="Faturamento por tipo pagamento")
 col4.plotly_chart(fig_kind, use_container_width= True)
+
+avaliacao_total = df_filter.groupby("City")[("Rating")].mean().reset_index()
+fig_avaliacao = px.bar(city_total, x="City", y="Rating", barmode="group", title="Avaliação")
+col5.plotly_chart(fig_avaliacao, use_container_width= True)
